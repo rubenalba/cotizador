e2b2cc4f6689fdf615af91c7db45b503bbdc8f3b
Rubén Alba <ruben.alba@birchmangroup.com>
2021-03-03 04:33:21 +0100

    nuevo proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 function App() {
   return (
-    <Header titulo="Cotizador de Seguros"/>
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario/> 
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
